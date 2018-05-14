f647b95003132be121a64c80e09d497e24e5117a
UsernameDiana <ms.strele@gmail.com>
2018-05-14 14:26:32 +0200

    mongodb connector

--- WebAppGUI/WebApp/src/main/java/databaseAccess/MongoDBConnector.java
+++ WebAppGUI/WebApp/src/main/java/databaseAccess/MongoDBConnector.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package databaseAccess;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 
 /**
  *
  * @author diana
  */
 public class MongoDBConnector {
-    
+
+    private MongoClient mongoClient = null;
+
+    private String URI = "mongodb://place your mongodb access here";
+
+    public MongoClient getConnection() {
+        try {
+            this.mongoClient = new MongoClient(new MongoClientURI(URI));
+
+        } catch (Exception e) {
+            System.out.println("ERROR IN MONGODB CONNECTION" + e.toString());
+        }
+
+        return this.mongoClient;
+    }
+
+    public void close() {
+        this.mongoClient.close();
+    }
+
 }
