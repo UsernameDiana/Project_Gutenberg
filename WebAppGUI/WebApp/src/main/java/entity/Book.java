0fe3cb6aca6839bfd58a277ee198200359da849c
UsernameDiana <ms.strele@gmail.com>
2018-05-14 14:17:41 +0200

    entity class added

--- WebAppGUI/WebApp/src/main/java/entity/Book.java
+++ WebAppGUI/WebApp/src/main/java/entity/Book.java
@@ -12,5 +12,41 @@ public class Book {
     String title;
     String author;
     List<City> cities;
-    
+
+    public Book() {
+
+    }
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(String title, String author, List<City> cities) {
+        this.title = title;
+        this.author = author;
+        this.cities = cities;
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public List<City> getCities() {
+        return cities;
+    }
+
 }
--- WebAppGUI/WebApp/src/main/java/entity/City.java
+++ WebAppGUI/WebApp/src/main/java/entity/City.java
@@ -10,4 +10,38 @@ public class City {
     private double lon;
     private double lat;
 
+    public City() {
+
+    }
+
+    public City(String name, double lat, double lon) {
+        this.name = name;
+        this.lon = lon;
+        this.lat = lat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
 }
