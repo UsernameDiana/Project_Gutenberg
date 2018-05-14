c748f4891da3738e20d03851fbabad8a769da666
UsernameDiana <ms.strele@gmail.com>
2018-05-14 14:07:28 +0200

    architecture

--- WebAppGUI/WebApp/src/main/java/entity/City.java
+++ WebAppGUI/WebApp/src/main/java/entity/City.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entity;
 
 /**
@@ -10,5 +5,9 @@ package entity;
  * @author diana
  */
 public class City {
-    
+
+    private String name;
+    private double lon;
+    private double lat;
+
 }
