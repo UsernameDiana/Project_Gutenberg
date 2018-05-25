106194d95c87c3838d017fd473da76fb322eac5f
UsernameDiana <ms.strele@gmail.com>
2018-05-25 12:27:14 +0200

    selenium setup

--- WebAppGUI/WebApp/src/test/java/stepdefs/StepDefinitions.java
+++ WebAppGUI/WebApp/src/test/java/stepdefs/StepDefinitions.java
@@ -8,7 +8,6 @@ package stepdefs;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
