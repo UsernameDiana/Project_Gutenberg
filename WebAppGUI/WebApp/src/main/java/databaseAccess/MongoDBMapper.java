20ca2b9153bc043fac4711e1e286e242f950c08c
UsernameDiana <ms.strele@gmail.com>
2018-05-27 17:53:16 +0200

    mongo queries

--- WebAppGUI/WebApp/src/main/java/databaseAccess/MongoDBMapper.java
+++ WebAppGUI/WebApp/src/main/java/databaseAccess/MongoDBMapper.java
@@ -5,15 +5,16 @@
  */
 package databaseAccess;
 
-import Interfaces.IDataAccess;
+import Interfaces.IBook;
+import Interfaces.ICity;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Filters.in;
 import entity.Book;
-import Interfaces.IBook;
-import entity.City;
 import java.util.ArrayList;
 import java.util.List;
 import org.bson.Document;
@@ -27,47 +28,54 @@ public class MongoDBMapper {
     private MongoDBConnector mongoCon;
     private static MongoClient con = null;
     private String dbname = "gutenberg";
-    private String col = "books";
+    private String bcol = "books";
 
     public MongoDBMapper() {
-        this.mongoCon = mongoCon;
+        this.mongoCon = new MongoDBConnector();
         this.con = mongoCon.getConnection();
     }
 
-    MongoDBMapper(MongoDBConnector dbcmongo) {
-        this.mongoCon = mongoCon;
-        this.con = mongoCon.getConnection();
-    }
+    public List<IBook> getBooksByCityName(String cityName) {
 
-    
-
-    public List<Book> getBooksByCityName(String cityName) {
-
-
-        List<Book> list = new ArrayList();
+        List<IBook> books = new ArrayList();
 
         try {
             MongoDatabase database = con.getDatabase(dbname);
-            MongoCollection coll = database.getCollection(col);
+            MongoCollection coll = database.getCollection(bcol);
             FindIterable<Document> findIterable = coll.find(in("city.name", cityName));
-
             for (Document document : findIterable) {
-                System.out.println(document.toJson());
+                String jsonStr = document.toJson();
+                System.out.println(jsonStr);
+                books.add((IBook) document);
             }
 
         } catch (Exception e) {
             System.out.println(e.toString());
         }
-        return list;
+        return books;
     }
 
-    
-    public List<City> getCitiesByBookTitle(String bookTitle) {
-        throw new UnsupportedOperationException("Not supported yet.");
-        //To change body of generated methods, choose Tools | Templates.
-    }
+    public List<ICity> getCitiesByBookTitle(String bookTitle) {
 
-    
+        List<ICity> cities = new ArrayList();
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            MongoDatabase database = con.getDatabase(dbname);
+            MongoCollection coll = database.getCollection(bcol);
+            FindIterable<Document> findIterable = coll.find(eq("bookTitle", bookTitle));
+            for (Document document : findIterable) {
+                String jsonStr = document.toJson();
+                IBook b = mapper.readValue(jsonStr, Book.class);
+                for (ICity c : b.getCities()) {
+                    cities.add(c);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return cities;
+    }
 
     public List<Book> getBooksByAuthorName(String authorName) {
 
