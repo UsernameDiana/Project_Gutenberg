1dbe1857cf3737cdf50ea1a770e53f6121541e4f
UsernameDiana <ms.strele@gmail.com>
2018-05-27 18:13:47 +0200

    mongo test

--- WebAppGUI/WebApp/src/test/java/mongoUnitTests/MongoUnitTests.java
+++ WebAppGUI/WebApp/src/test/java/mongoUnitTests/MongoUnitTests.java
@@ -3,23 +3,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dataUnitTests;
+package mongoUnitTests;
 
-import databaseAccess.MongoDBMapper;
-import static org.hamcrest.CoreMatchers.equalTo;
-import junitparams.FileParameters;
 import Interfaces.IBook;
-import java.util.ArrayList;
+import databaseAccess.MongoDBMapper;
+import entity.Book;
 import java.util.List;
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
 import org.hamcrest.CoreMatchers;
+import static org.hamcrest.CoreMatchers.equalTo;
+import org.hamcrest.Matchers;
 import static org.junit.Assert.assertThat;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  *
  * @author diana
  */
+@RunWith(JUnitParamsRunner.class)
 public class MongoUnitTests {
 
     private MongoDBMapper MongoDBMapper;
@@ -31,10 +35,9 @@ public class MongoUnitTests {
 
     @Test
     @FileParameters("src/test/resources/UserStory1_mongodb.csv")
-    public void getBooksByCityTest(String city, String title, String author) {
-        List<IBook> books = new ArrayList();
-        books = MongoDBMapper.getBooksByCityName(city);
+    public void testGetBooksByCity(String city, String title, String author) {
+        List<IBook> books = MongoDBMapper.getBooksByCityName(city);
+        assertThat(books, Matchers.<Book>hasItem(Matchers.hasProperty("bookTitle", equalTo(title))));
         assertThat(books.get(0).getTitle(), CoreMatchers.is(equalTo(title)));
-        //assertThat(books.get(0).getAuthor(), CoreMatchers.is(equalTo(author)));
     }
 }
