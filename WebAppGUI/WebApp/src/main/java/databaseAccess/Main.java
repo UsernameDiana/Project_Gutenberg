00ec80c0fd7dd218179aa0e73bc9b7ae33ab6924
darkSideMatters <plamengetsov95@gmail.com>
2018-05-28 11:38:24 +0200

    PostG last

--- WebAppGUI/WebApp/src/main/java/databaseAccess/Main.java
+++ WebAppGUI/WebApp/src/main/java/databaseAccess/Main.java
@@ -10,6 +10,10 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import entity.Book;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.bson.Document;
 
@@ -18,16 +22,22 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
 //        SQLDBMapper sqlCon = new SQLDBMapper(new SQLDBConnector());
-        MongoDBConnector con = new MongoDBConnector();
-        MongoDatabase mongodb = con.getDatabase();
-        MongoDBMapper mMap = new MongoDBMapper();
+//        MongoDBConnector con = new MongoDBConnector();
+//        MongoDatabase mongodb = con.getDatabase();
+//        MongoDBMapper mMap = new MongoDBMapper();
+//        PostgDBConnector postgconn = new PostgDBConnector();
+//        Connection con = postgconn.getConnection();
+//        PostgDBMapper postgCon = new PostgDBMapper();
+//        System.out.println("books" + postgCon.getBooksByCityName("Paris", con));
+        
+        
 //        MongoCollection<Document> collection = mongodb.getCollection("books");
 
 //        BasicDBObject searchQuery = new BasicDBObject();
 //        searchQuery.put("title", "Within the Tides");
 //        System.out.println("Books: " + mMap.getBooksByAuthorName("Herbert George Wells"));
 //        System.out.println("Books: " + mMap.getBooksByAuthorName("Anonymous"));
-        System.out.println("Cities: " + mMap.getCitiesByBookTitle("Apocolocyntosis"));
+//        System.out.println("Cities: " + mMap.getCitiesByBookTitle("Apocolocyntosis"));
 
 //        MongoCursor<Document> cursor = collection.find(searchQuery).iterator();
 //        try {
@@ -45,6 +55,12 @@ public class Main {
 //        System.out.println("------------ 1st Query");
 //        for (IBook iBook : blist) {
 //            System.out.println(iBook.getTitle());
+////        }
+//        Map<Long, Book> bblist = new HashMap();
+//        bblist = postgCon.getBooksByCityName("Paris",);
+//        System.out.println("------------ 1st Query");
+//        for (Book Book : bblist) {
+//            System.out.println(Book.getTitle());
 //        }
 //
 //        System.out.println("------------ 2nd Query");
